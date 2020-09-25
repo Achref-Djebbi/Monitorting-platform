bff97ffef2f6b427620770cfbf34b75baa8bff13
Achref <dj.achref@>
2020-09-25 17:12:05 +0100

    adding components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/headerComponent/header';
+import Footer from './components/footerComponent/footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+     <Header></Header>
         <p>Hello World</p>
-      </header>
+     <Footer></Footer>
+      
     </div>
   );
 }
---
+++ src/components/headerComponent/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./header.css";
+
+function Header() {
+  return (
+    <div className="Header">
+     
+        <p>Header</p>
+      
+    </div>
+  );
+}
+
+export default Header;
